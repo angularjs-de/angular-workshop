b413a29eb3722dc0f08c102d58e761bde261211f
Peter <baggersio@icloud.com>
2017-02-07 10:35:50 +0100

    Create an Info-Box

---
+++ src/app/info-box/info-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'info-box',
+  templateUrl: './info-box.component.html',
+  styleUrls: ['./info-box.component.css']
+})
+export class InfoBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
