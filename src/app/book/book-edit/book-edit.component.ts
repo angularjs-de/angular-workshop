96e4f4f43f6004d390740c1741704ef36b0d9e2d
Peter <baggersio@icloud.com>
2017-02-07 12:43:33 +0100

    Extend the BookEdit Component

--- src/app/book/book-edit/book-edit.component.ts
+++ src/app/book/book-edit/book-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
+import { ActivatedRoute } from '@angular/router';
+import { BookDataService } from '../shared/book-data.service';
+import { mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'book-edit',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookEditComponent implements OnInit {
 
-  constructor() { }
+  book: Book;
+
+  constructor(private route: ActivatedRoute, private bookService: BookDataService) { }
 
   ngOnInit() {
+    this.route.params.pipe(
+      mergeMap((params: { isbn: string }) => this.bookService.getBookByIsbn(params.isbn))
+    )
+      .subscribe(book => this.book = book);
+  }
+
+  onSubmit(value) {
+
+    console.log(value);
   }
 
 }
