34573907fd376caadadc1a86beccf454ad3b0196
Peter <baggersio@icloud.com>
2017-02-07 10:45:44 +0100

    Create a title @Input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { InfoBoxComponent } from './info-box/info-box.component';
 import { MouseCursorComponent } from './mouse-cursor/mouse-cursor.component';
+import { TitleBoxComponent } from './title-box/title-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InfoBoxComponent,
-    MouseCursorComponent
+    MouseCursorComponent,
+    TitleBoxComponent
   ],
   imports: [
     BrowserModule,
