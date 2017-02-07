7e45e4c582be9f0896e67013c33a298678ab5f69
Peter <baggersio@icloud.com>
2017-02-07 10:35:50 +0100

    Create an Info-Box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { InfoBoxComponent } from './info-box/info-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoBoxComponent
   ],
   imports: [
     BrowserModule,
