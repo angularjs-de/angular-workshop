6ae73a0829970487d98311f565909f99d9e60161
Peter <baggersio@icloud.com>
2017-02-07 13:37:58 +0100

    Tooltip Directive I

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
 import { ShoutPipe } from './shared/shout.pipe';
+import { TooltipDirective } from './shared/tooltip.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShoutPipe,
+    TooltipDirective,
   ],
   imports: [
     BrowserModule,
