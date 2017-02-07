e9e3849d7521271512336ba641a99e2135a18933
Peter <baggersio@icloud.com>
2017-02-07 10:40:01 +0100

    Output mouse cursor position

---
+++ src/app/mouse-cursor/mouse-cursor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mouse-cursor',
+  templateUrl: './mouse-cursor.component.html',
+  styleUrls: ['./mouse-cursor.component.css']
+})
+export class MouseCursorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
