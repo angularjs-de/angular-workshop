9739165b430ec90aa45d1db43af51b27c537aa0e
Peter <baggersio@icloud.com>
2017-02-07 11:26:55 +0100

    Add routing file

---
+++ src/app/app.routing.ts
@@ -0,0 +1,9 @@
+import { Routes, RouterModule } from '@angular/router';
+import { BookListComponent } from './book-list/book-list.component';
+
+export const routes: Routes = [{
+  path: 'books',
+  component: BookListComponent
+}];
+
+export const routing = RouterModule.forRoot(routes);
