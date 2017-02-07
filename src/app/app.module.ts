4ca05fbe085d04faa7126cb6dc2c0b4af3ca612a
Peter <baggersio@icloud.com>
2017-02-07 10:40:01 +0100

    Output mouse cursor position

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { InfoBoxComponent } from './info-box/info-box.component';
+import { MouseCursorComponent } from './mouse-cursor/mouse-cursor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InfoBoxComponent
+    InfoBoxComponent,
+    MouseCursorComponent
   ],
   imports: [
     BrowserModule,
