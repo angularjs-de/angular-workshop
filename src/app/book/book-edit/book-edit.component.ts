31cde499bd491018e3caac5ea72eff7a03f6b2d3
Peter <baggersio@icloud.com>
2017-02-07 12:26:41 +0100

    Create a BookEdit Component

---
+++ src/app/book/book-edit/book-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'book-edit',
+  templateUrl: './book-edit.component.html',
+  styleUrls: ['./book-edit.component.css']
+})
+export class BookEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
